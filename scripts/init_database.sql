be1233d9bce9fc979c9e20ea16c38a3b40d2036e
PrinceSiboniso <118721784+PrinceSiboniso@users.noreply.github.com>
2025-03-29 01:51:32 +0200

    Update and rename init_dtabase.sql to init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,13 @@
+-- Create Database 'DataWareHouse'
+
+USE master;
+
+CREATE DATABASE DataWarehouse;
+
+USE DataWarehouse;
+
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
